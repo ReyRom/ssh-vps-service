af7c869770404db48af9c1a9589721ff68a925c0
ReyRom <s03roman@mail.ru>
2024-09-12 19:36:52 +0300

    Добавьте файлы проекта.

---
+++ ssh-vps-service/Worker.cs
@@ -0,0 +1,39 @@
+using System.Diagnostics;
+
+namespace ssh_vps_service
+{
+    public class Worker : BackgroundService
+    {
+        private readonly Process process = new System.Diagnostics.Process();
+
+        public Worker()
+        {
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.Arguments = "/c ssh -NC -D 8888 vps";
+            process.StartInfo.CreateNoWindow = true;
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            process.Start();
+            return base.StartAsync(cancellationToken);
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            process.Kill();
+            return base.StopAsync(cancellationToken);
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                Console.WriteLine(await process.StandardOutput.ReadLineAsync());
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
